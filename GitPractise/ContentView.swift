21797ec152ef8f8dd10b87453d586d9e35d93839
rithiksinha <rithiksinha2000@gmail.com>
2021-11-07 18:15:49 +0530

    Edit File

--- GitPractise/ContentView.swift
+++ GitPractise/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                Text ("Rithik Sinha")
+                    .padding()
+                    .frame(width: UIScreen.main.bounds.width, height: 50, alignment: .center)
+                    .font(.system(size: 50))
+                    .foregroundColor(Color.white)
+                    .cornerRadius(10)
+                    .padding()
+                
+                HStack {
+                    Text ("Welcome Back")
+                        .foregroundColor(Color.white)
+                        .font(.system(size: 25))
+                }
+                
+            }.padding(.horizontal, 10)
+            
+            
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13")
     }
 }
