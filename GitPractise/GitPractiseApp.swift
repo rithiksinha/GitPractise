c2792c44d5176679a2f41d1bd2297011cbb0e82b
rithiksinha <rithiksinha2000@gmail.com>
2021-11-07 17:59:07 +0530

    Initial Commit

---
+++ GitPractise/GitPractiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPractiseApp.swift
+//  GitPractise
+//
+//  Created by Ritik Sinha on 07/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GitPractiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
